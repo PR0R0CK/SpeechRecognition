315d47edaa638a58e8068645bd9c44222858889b
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-11-19 16:18:43 +0100

    start

--- App.py
+++ App.py
@@ -1,1 +1,12 @@
-print('Hello world');
+import speech_recognition as sr
+
+print(sr.__version__)
+r = sr.Recognizer()
+# r.recognize_google();
+
+
+harvard = sr.AudioFile('harvard.wav')
+with harvard as source:
+    audio = r.record(source)
+    type(audio)
+r.recognize_google(audio)
