eb55c09e862234f1df3cfc3dad627be66f8ed08a
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-11-20 09:55:43 +0100

    functions

--- App.py
+++ App.py
@@ -3,14 +3,28 @@ import speech_recognition as sr
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-## listening
-# with mic as source:
-#     audio = r.listen(source)
-#
-# print(r.recognize_google(audio))
 
-## listening with adjusting noises
-# with mic as source:
-#     r.adjust_for_ambient_noise(source)
-#     audio = r.listen(source)
-# print(r.recognize_google(audio))
+# listening
+def listening():
+    with mic as source:
+        audio = r.listen(source)
+
+    print(r.recognize_google(audio))
+
+
+# listening with adjusting noises
+def listening_with_noises():
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        print('Say something...')
+        audio = r.listen(source)
+    print(r.recognize_google(audio))
+
+
+def main():
+    listening()
+    # listening_with_noises()
+
+
+if __name__ == '__main__':
+    main()
